20ba6c60de400b9aa71042bf09e5e9461ec4dbed
David Rymer <david-rymer@hotmail.co.uk>
2018-12-06 11:26:15 +0000

    Fixed an issue with finding the grades

--- JobDiary/scraper/dictionary.py
+++ JobDiary/scraper/dictionary.py
@@ -1,5 +1,5 @@
 def check_grade(html):
-    combinations = ["1st", "First class", "1st class", "Upper second class", "2:1", "2:2"]
+    combinations = ["Upper second class", "2:1", "2:2", "1st", "First class", "1st class"]
 
     for classification in combinations:
         if classification.lower() in html.replace("first class results.", ""):
